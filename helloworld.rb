64e9109a37919d386ef810f3417041e96777608c
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-24 13:47:09 +0000

    [Pause at] 28:34

--- helloworld.rb
+++ helloworld.rb
@@ -1,1 +1,13 @@
 print "Hello World"
+puts "Test" # put = print with new line
+
+# variable
+name = "Yuu"
+age = "18"
+# #{variable name}
+puts "My name is #{name} and I am #{age} years old"
+# you can update variable and variable value == last line
+name = "Eris"
+age = '"Secret"'
+puts "Her name is #{name} and she #{age} years old"
+# expect output "Her name is Eris and she "Secret" years old"
