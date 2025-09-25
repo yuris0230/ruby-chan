366f834b565e9ae73dc639d89c9f6e22f456e798
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-25 14:59:33 +0000

    [Pause at] 37:45

---
+++ datatype.rb
@@ -0,0 +1,7 @@
+# :string data type typically defaults to a limit of 255 chars
+# :text are longer strings (30k+ chars depending on the database system)
+name = "Yuris"
+age = 18 # int without "" if include "number" theres gonna be string
+gpa = 3.2 # float
+ismale = true # boolean true of faluse
+flaws = nil # nil basically means that it doesn't have a value
---
+++ methods.rb
@@ -0,0 +1,8 @@
+# method is essentially just a little block of code that we can call
+# and it will either modify our string or give us information about our string
+
+# if you wanna add " inside of "" just type \"
+# \n for new line just like in c
+# name = container that contain value
+name = "Yuris\"50th Canvas"
+puts name 
