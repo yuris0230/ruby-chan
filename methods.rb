1644d66a8fa470bae22384bae62ce147291fcd91
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-28 14:15:34 +0000

    [Pause at] 1:35:30

--- methods.rb
+++ methods.rb
@@ -1,24 +1,9 @@
-# method is essentially just a little block of code that we can call
-# and it will either modify our string or give us information about our string
+# methods is just a block of code that we can write (functions)
+# def means define
+def sayhi(name, job, age) # (parameter)
+  puts "Hello!! #{name} you are the #{job} and you are #{age.to_s}"
+end
 
-# if you wanna add " inside of "" just type \"
-# \n for new line just like in c
-# name = container that contain value
-name = "Yuris\"50th Canvas"
-# upcase, downcase, strip -> delete weird white spaces
-puts name.upcase() # .upcase is the method that make string or text upper case
-puts "programming".upcase()
-
-# length -> count char, include? -> return true or false
-puts name.include? "50th"
-# expect output = true
-
-# [n] print the char from index (start from 0)
-puts name[0,3] # give you first 3 character (0,1,2) *not include 3
-# expect output "Yur"
-
-# only first character that found
-puts name.index("Yuris")
-# expect output 0
-
-puts "His handle name is #{name.upcase} #{name.length}"
+sayhi("PoopMcdingleFart", "Boss", 18)
+# if not pass all the parameter thats gonna be error, so
+# def sayhi(name="User", Job="Adventure", age="XXX") <- call {default value}
