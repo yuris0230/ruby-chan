c43f7f9ba0f85ade2e473412b6dadb740aa94df6
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-10-07 14:13:58 +0000

    [Finished]

---
+++ inteactive.rb
@@ -0,0 +1,6 @@
+# inteactive ruby (irb)
+# a little app that we can run from terminal
+# essentially allows us to test out and use different ruby commands (sort of sandbox environment)
+# irb -v for check version
+# irb for use
+# not nesessary
